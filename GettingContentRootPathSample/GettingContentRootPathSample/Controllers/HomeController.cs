b9f80d972cb3fe151337c2a0624b3318d347a82c
Hiromasa-Masuda <hiromasa.masuda@gmail.com>
2017-04-11 00:05:18 +0900

    ASP.NET Core MVC Getting Content Root Path Sample

---
+++ GettingContentRootPathSample/GettingContentRootPathSample/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using GettingContentRootPathSample.Models;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace GettingContentRootPathSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// IHostingEnvironment のインスタンスを保持
+        /// </summary>
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment = null;
+
+        /// <summary>
+        /// コンストラクター経由で、IHostingEnvironment のインスタンスを取得
+        /// </summary>
+        /// <param name="hostingEnvironment"></param>
+        public HomeController(Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            //IHostingEnvironment をフィールドに保持
+            this._hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            //シリアライザーのインスタンスを生成
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Person));
+
+            string filePath = Path.Combine(this._hostingEnvironment.ContentRootPath, "Files", "person.json");
+            
+            Person deSerializedPerson = null;
+
+            //入力ファイル ストリームの生成
+            //using (FileStream stream = new FileStream(filePath, FileMode.Open))
+            using (Stream stream = System.IO.File.Open(filePath, FileMode.Open))
+            {
+                //逆シリアライズ
+                deSerializedPerson = jsonSerializer.ReadObject(stream) as Person;
+            }
+
+            return View(deSerializedPerson);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ GettingContentRootPathSample/GettingContentRootPathSample/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace GettingContentRootPathSample.Models
+{
+    [DataContract]
+    internal class Person
+    {
+        [DataMember]
+        internal string Name { get; set; }
+
+        [DataMember]
+        internal int Age { get; set; }
+    }
+}
